d4881ef4fc0290de66784df0706a7ab2960f2745
iamfaijank <iamfaijank@gmail.com>
2026-07-07 12:07:01 +0530

    refactor: reorder filters in sahayog_ticket_report.js to move date filters to top

--- sahayog_ticket/sahayog_ticket/report/sahayog_ticket_report/sahayog_ticket_report.js
+++ sahayog_ticket/sahayog_ticket/report/sahayog_ticket_report/sahayog_ticket_report.js
@@ -2,6 +2,20 @@ frappe.query_reports["Sahayog Ticket Report"] = {
   // Filter definitions for report data filtering
   filters: [
     {
+      fieldname: "from_date",
+      label: "From Date",
+      fieldtype: "Date",
+      default: frappe.datetime.add_days(frappe.datetime.nowdate(), -30),
+      reqd: 1,
+    },
+    {
+      fieldname: "to_date",
+      label: "To Date",
+      fieldtype: "Date",
+      default: frappe.datetime.nowdate(),
+      reqd: 1,
+    },
+    {
       fieldname: "status",
       label: "Status",
       fieldtype: "Select",
@@ -65,20 +79,6 @@ frappe.query_reports["Sahayog Ticket Report"] = {
       label: "Assigned To",
       fieldtype: "Link",
       options: "User",
-    },
-    {
-      fieldname: "from_date",
-      label: "From Date",
-      fieldtype: "Date",
-      default: frappe.datetime.add_days(frappe.datetime.nowdate(), -30),
-      reqd: 1,
-    },
-    {
-      fieldname: "to_date",
-      label: "To Date",
-      fieldtype: "Date",
-      default: frappe.datetime.nowdate(),
-      reqd: 1,
     },
   ],
 };
