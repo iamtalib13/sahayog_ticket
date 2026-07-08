5678423f4ead6da84372ffd68d402e1bde712393
iamfaijank <iamfaijank@gmail.com>
2026-07-08 16:48:30 +0530

    feat: set default from_date to first day of current month

--- sahayog_ticket/sahayog_ticket/report/sahayog_ticket_report/sahayog_ticket_report.js
+++ sahayog_ticket/sahayog_ticket/report/sahayog_ticket_report/sahayog_ticket_report.js
@@ -226,7 +226,7 @@ frappe.query_reports["Sahayog Ticket Report"] = {
       fieldname: "from_date",
       label: "From Date",
       fieldtype: "Date",
-      default: frappe.datetime.add_days(frappe.datetime.nowdate(), -30),
+      default: frappe.datetime.month_start(),
       reqd: 1,
     },
     {
